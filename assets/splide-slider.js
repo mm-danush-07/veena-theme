fe31be072ba44e3ea4d4a7504e1e3c64a30871aa
danush07 <danushhari2001@gmail.com>
2024-02-13 19:19:17 +0530

    Added Hero Banner Section

---
+++ assets/splide-slider.js
@@ -0,0 +1,16 @@
+class SplideSlider extends HTMLElement {
+  constructor() {
+    super();
+    this.splideEl = this.querySelector(".splide");
+    this.options = JSON.parse(this.querySelector("script").textContent);
+
+    console.log(this.options);
+    this.mountSplider();
+  }
+
+  mountSplider() {
+    new Splide(this.splideEl, this.options).mount();
+  }
+}
+
+customElements.define("slide-slider", SplideSlider);
